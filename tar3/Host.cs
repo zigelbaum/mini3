62f1264b16696478d119ca578f0907925f2c7dd8
zigelbaum <Chanush850@gmail.com>
2019-12-01 17:38:48 +0200

    step 6

---
+++ tar3/Host.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tar3
+{
+    class Host
+    {
+        private string HostName;
+        private List<HostingUnit> Units;
+
+        public string HostName1 { get => HostName; set => HostName = value; }
+        internal List<HostingUnit> Units1 { get => Units; set => Units = value; }
+    }
+}
---
+++ tar3/HostingUnit.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tar3
+{
+    class HostingUnit
+    {
+        private string UnitName;
+        private int Rooms;
+        private bool IsSwimmingPool;
+        public List <DateTime> AllOrders { get; set; }
+        public List <string> Uris { get; set; }
+
+        public string UnitName1 { get => UnitName; set => UnitName = value; }
+        public int Rooms1 { get => Rooms; set => Rooms = value; }
+        public bool IsSwimmingPool1 { get => IsSwimmingPool; set => IsSwimmingPool = value; }
+    }
+}
